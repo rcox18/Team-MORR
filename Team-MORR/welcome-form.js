461269e56239e3e8fbef61d86622f842890698ad
dystopiceyre <livvy16.olivia@gmail.com>
2019-10-29 23:56:38 -0700

    JS validation for welcome page

---
+++ Team-MORR/welcome-form.js
@@ -0,0 +1,34 @@
+document.getElementById("welcome-form").onsubmit = validate;
+const requiredInputErrs = document.getElementsByClassName("required-inputErr");
+const btnSubmit = document.getElementById("submit");
+
+for (let i = 0; i < requiredInputErrs.length; i++)
+{
+    requiredInputErrs[i].style.visibility = "hidden";
+}
+
+addEventListener("change", function() {
+    if ($("#gender").value == "other") {
+        $("#other-gender").toggle();
+    }
+    });
+
+function validate() {
+    let isValid = true;
+
+    let requiredInputValues = document.getElementsByClassName("required-input");
+
+    for (let i = 0; i < requiredInputValues.length; i++) {
+
+        if (requiredInputValues[i].value === "") {
+            requiredInputValues[i].classList.add("border-danger");
+            requiredInputErrs[i].style.visibility = "visible";
+            isValid = false;
+        } else {
+            requiredInputValues[i].classList.remove("border-danger");
+            requiredInputErrs[i].style.visibility = "hidden";
+        }
+    }
+
+    return isValid;
+}}
