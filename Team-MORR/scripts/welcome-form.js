0685046a2e970223d3b1b38ee255749f4917f69d
rcox18 <55718398+rcox18@users.noreply.github.com>
2019-10-30 22:34:34 -0700

    Update welcome-form.js

--- Team-MORR/scripts/welcome-form.js
+++ Team-MORR/scripts/welcome-form.js
@@ -8,54 +8,57 @@
 */
 
 document.getElementById("welcome-form").onsubmit = validate;
-const fName = document.getElementById("firstName");
-const lName = document.getElementById("lastName");
+const fName = document.getElementById("first-name");
+const lName = document.getElementById("last-name");
 const phone = document.getElementById("phone");
 const email = document.getElementById("email");
-const dob = document.getElementById("dob");
-const race = document.getElementById("race-dropdown");
-const otherRace = document.getElementById("other-race-div");
-const raceErr = document.getElementById("race-err");
+const dob = document.getElementById("date-of-birth");
 const gradYear = document.getElementById("graduation-year");
 const fNameErr = document.getElementById("fName-err");
 const lNameErr = document.getElementById("lName-err");
 const dobErr = document.getElementById("dob-err");
-const gender = document.getElementById("gender");
 const genderErr = document.getElementById("gender-err");
 const emailErr = document.getElementById("email-err");
 const phoneErr = document.getElementById("phone-err");
-const gradYearErr = document.getElementById("gradYear-err");
+const gradYearErr = document.getElementById("grad-year-err");
+const otherGenderText = document.getElementById("other-gender");
+const requiredInputValues = document.getElementsByClassName("text-danger err");
+const gender = document.getElementById("gender");
+const genderOptions = document.getElementsByClassName("gender-option");
+const raceSelector = document.getElementById("race-selector");
+const raceOptions = document.getElementsByClassName("race-option");
+const otherRaceText = document.getElementById("other-race");
 
-
+// from: https://www.regular-expressions.info/email.html
 const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
 const phoneRegex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
 
-const btnSubmit = document.getElementById("submit");
+otherGenderText.style.display = "none";
+otherRaceText.style.display = "none";
 
-gender.addEventListener("change", function() {
-    if (gender.value === "other") {
-        document.getElementById("other-gender").style.display = "block";
+for(let i = 0; i < requiredInputValues.length; i++) {
+    requiredInputValues[i].style.display = "none";
+}
+
+gender.onchange =  function() {
+    if (genderOptions[gender.selectedIndex].value === "other") {
+        otherGenderText.style.display = "block";
     } else {
-        document.getElementById("other-gender").style.display = "none";
+        otherGenderText.style.display = "none";
     }
-    });
+};
 
-race.addEventListener("change", function() {
-    if (race.value === "other") {
-        otherRace.style.display = "block";
+raceSelector.onchange =  function() {
+    if (raceOptions[raceSelector.selectedIndex].value === "other") {
+        otherRaceText.style.display = "block";
     } else {
-        otherRace.style.display = "none";
+        otherRaceText.style.display = "none";
     }
-    });
-
+};
 
 function validate() {
-    let isValid = true;
 
-    var requiredInputValues = document.getElementsByClassName("text-danger err");
-    for(i = 0; i < requiredInputValues.length; i++) {
-        requiredInputValues[i].style.display = "none";
-    }
+    let isValid = true;
 
     if(fName.value === "") {
         fNameErr.style.display = "block";
@@ -84,10 +87,6 @@ function validate() {
     if(gender.value === "none") {
         genderErr.style.display = "block";
         isValid = false;
-    }
-    if(race.value === "none") {
-        raceErr.style.display = "block";
-       isValid = false;
     }
 
     return isValid;
