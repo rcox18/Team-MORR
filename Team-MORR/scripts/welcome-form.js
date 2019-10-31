c6fcd9d6d73f31f031b20eadf7cd26a2e2a54206
dystopiceyre <livvy16.olivia@gmail.com>
2019-10-30 20:38:24 -0700

    -Added race validation TODO: -change date input so it works on all browsers

--- Team-MORR/scripts/welcome-form.js
+++ Team-MORR/scripts/welcome-form.js
@@ -13,23 +13,24 @@ const lName = document.getElementById("lastName");
 const phone = document.getElementById("phone");
 const email = document.getElementById("email");
 const dob = document.getElementById("dob");
+const race = document.getElementById("race-dropdown");
+const otherRace = document.getElementById("other-race-div");
+const raceErr = document.getElementById("race-err");
 const gradYear = document.getElementById("graduation-year");
 const fNameErr = document.getElementById("fName-err");
 const lNameErr = document.getElementById("lName-err");
 const dobErr = document.getElementById("dob-err");
+const gender = document.getElementById("gender");
 const genderErr = document.getElementById("gender-err");
 const emailErr = document.getElementById("email-err");
 const phoneErr = document.getElementById("phone-err");
 const gradYearErr = document.getElementById("gradYear-err");
 
 
-
 const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
 const phoneRegex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
 
-const gender = document.getElementById("gender");
 const btnSubmit = document.getElementById("submit");
-
 
 
 gender.addEventListener("change", function() {
@@ -39,6 +40,15 @@ gender.addEventListener("change", function() {
         document.getElementById("other-gender").style.display = "none";
     }
     });
+
+race.addEventListener("change", function() {
+    if (race.value ==== "other") {
+        otherRace.style.display = "block";
+    } else {
+        otherRace.style.display = "none";
+    }
+    });
+
 
 function validate() {
     let isValid = true;
@@ -75,6 +85,10 @@ function validate() {
     if(gender.value === "none") {
         genderErr.style.display = "block";
         isValid = false;
+    }
+    if(race.value === "none") {
+        raceErr.style.display = "block";
+       isValid = false;
     }
 
     return isValid;
