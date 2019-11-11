ccfac5998a59d7c1c40a313d17127e970230e762
rmcox <gr8npowerful@yahoo.com>
2019-11-11 10:16:06 -0800

    bugs

--- Team-MORR/pages/welcome-confirmation-page.php
+++ Team-MORR/pages/welcome-confirmation-page.php
@@ -239,7 +239,7 @@ if (!empty($_POST)){
                 echo ($success ?  "<script>console.log('success');</script>" :
                 "<script>console.log('failure');</script>" );*/
             }else{
-                mysqli_error($cnxn);
+                echo mysqli_error($cnxn);
                 echo "there is a problem!";
             }
         return;
